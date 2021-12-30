48941f64ac1c6cc455946d3e38135f0b76b6f191
Jeong-lim <wjdfla401@naver.com>
2021-12-30 16:36:45 +0900

    instance

---
+++ Chapter6/src/EX6_1.java
@@ -0,0 +1,21 @@
+class EX6_1 {
+	public static void main(String[] args) {
+		Tv t;
+		t = new Tv();
+		t.channel = 7;
+		t.channelDOWN();
+		System.out.println("현재 채널은 " + t.channel + "입니다.");
+		
+
+	}
+}
+	
+class Tv {
+	String color;
+	boolean power;
+	int channel;
+	
+	void power() {power = !power; }
+	void channelUP() { ++channel; }
+	void channelDOWN() { --channel; }
+}
--- Chapter6/src/Hello2.java
+++ Chapter6/src/Hello2.java
@@ -1,9 +1,8 @@
-
-public class Hello2 {
-
+// 하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직
+public class Hello2 { // 소스파일 이름은 public class이름과 일치
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hello");
 	}
-
-}
+} 
+class Hello3{} // 하나의 소스파일에는 하나의 public class만 허용한다.
+class Hello4{}
