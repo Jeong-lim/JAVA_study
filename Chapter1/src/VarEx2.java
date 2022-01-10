ace3e301bc76eaa0243f192cfad3c636b224dacb
Jeong-lim <wjdfla401@naver.com>
2022-01-10 15:31:00 +0900

    설명 추가

--- Chapter1/src/VarEx2.java
+++ Chapter1/src/VarEx2.java
@@ -12,7 +12,10 @@ public class VarEx2 {
 		y = tmp;
 		
 		System.out.println("x: " + x + " y: " + y);
-
+		
+		
+		System.out.println("tmp의 값은?: " + tmp);
+		// 빈 공간 tmp를 이용해서 x에 저장된 값과 y에 저장된 값을 바꾸기
 	}
 
 }
